7448a9958f9f3208e7473fb3970e771467cfbb7b
WekesaN <isaacnyongesa2048@gmail.com>
2018-05-04 08:37:43 +0300

    add the js tags

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
-function display() {
-  var number = document.getElementsById("number").value;
-  for (var i = 0; i < number.length; i++) {
-    number[i]
+var number = parseInt(prompt("enter number"));
+var results = "";
+function display (){
+  for (var i = 1; i <= number; i++){
+  	console.log(i)
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+      results += "pingpong";
+    }
+    else if (i % 5 == 0) {
+      results += "pong";
+    }
+    else if (i % 3 == 0) {
+      results += "ping";
+    }
+    else {
+      results += i.toString();
+    }
+  };
   }
-  document.getElementsById("numbers").value = numbers;
-}
+  display();
+  alert(results);
+  
