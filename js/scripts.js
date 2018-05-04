dcdf0b25a240bf9bc9a528887a37430dd39be02b
WekesaN <isaacnyongesa2048@gmail.com>
2018-05-04 11:58:45 +0300

    add the rows and columns

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,40 @@
-var number = parseInt(prompt("enter number"));
-var results = "";
+var number;
 function display (){
   for (var i = 1; i <= number; i++){
   	console.log(i)
     if ((i % 3 == 0) && (i % 5 == 0)) {
-      results += "pingpong";
+      $('.number').append('pingpong<br/>');
     }
     else if (i % 5 == 0) {
-      results += "pong";
+      $('.number').append('pong<br/>');
     }
     else if (i % 3 == 0) {
-      results += "ping";
+      $('.number').append('ping<br/>');
     }
     else {
-      results += i.toString();
+      $('.number').append( i.toString() + "<br/>");
     }
   };
   }
-  display();
-  alert(results);
-  $(document).ready(function(){
-    $("form #btn").submit(function(event){
-      
-    })
-  })
+  //https://github.com/WekesaN/PINGPONG.git
+  // display();
+  // alert(results);
+  // $(document).ready(function(){
+  //   $("form").submit(function(event){
+  //     event.preventDefault()
+  //     var number = parseInt($("input#number").val());
+  //
+  //     var results = btn(number)
+  //     $("results").show()
+  //   })
+  // })
+  // user interface logic
+$(document).ready(function(){
+$("form").submit(function(event){
+number = $("input#number").val();
+display();
+
+event.preventDefault();
+});
+
+});
