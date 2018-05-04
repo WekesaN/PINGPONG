85fce3dc779ba03c23bc29086a3ef66d435ba5c7
WekesaN <isaacnyongesa2048@gmail.com>
2018-05-04 07:54:47 +0300

    add the name tags

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function display() {
+  var number = document.getElementsById("number").value;
+  for (var i = 0; i < number.length; i++) {
+    number[i]
+  }
+  document.getElementsById("numbers").value = numbers;
+}
